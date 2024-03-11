409de3a9ca20fe1b5c8f343ee5724654528aadca
Lisower <144545336+Lisower@users.noreply.github.com>
2024-03-11 15:25:23 +0300

    Update index.php

--- index.php
+++ index.php
@@ -42,8 +42,8 @@ if ($errors) {
 
 // Сохранение в базу данных.
 
-$user = 'db'; // Заменить на ваш логин uXXXXX
-$pass = '123'; // Заменить на пароль, такой же, как от SSH
+$user = 'u67447'; // Заменить на ваш логин uXXXXX
+$pass = '5579779'; // Заменить на пароль, такой же, как от SSH
 $db = new PDO('mysql:host=localhost;dbname=test', $user, $pass,
   [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]); // Заменить test на имя БД, совпадает с логином uXXXXX
 
