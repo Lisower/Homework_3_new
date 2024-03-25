dff245239348724b4dccd57e87164f1731146a6e
Lisower <144545336+Lisower@users.noreply.github.com>
2024-03-25 15:08:34 +0300

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ Button.addEventListener('click', () => {
 window.addEventListener('popstate', () => {
     Popup.style.display = 'none';
 });
- 
+ /*
 Form.addEventListener('submit', (e) => {
     e.preventDefault();
 
@@ -30,7 +30,7 @@ Form.addEventListener('submit', (e) => {
     };
     httpRequest.send(formData);
 });
-
+*/
 Form.addEventListener('change', () => {
     const formData = new FormData(Form);
     const data = Object.fromEntries(formData.entries());
