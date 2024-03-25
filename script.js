20f582193512e0cec2ea76d27a428b907c45457f
Lisower <144545336+Lisower@users.noreply.github.com>
2024-03-25 14:36:09 +0300

    Update script.js

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ Form.addEventListener('submit', (e) => {
 
     const formData = new FormData(Form);
     const httpRequest = new XMLHttpRequest();
-    httpRequest.open('POST', '/', true);
+    httpRequest.open('POST', '/Homework_3/Homework_3_new/index.php', true);
     httpRequest.onreadystatechange = () => {
         if (httpRequest.readyState === XMLHttpRequest.DONE) {
         if (httpRequest.status === 200) {
