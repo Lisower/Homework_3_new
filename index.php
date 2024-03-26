7750b8fe0bf53997d66131ca838c6e5554d99bfe
Lisower <144545336+Lisower@users.noreply.github.com>
2024-03-26 21:55:06 +0300

    Update index.php

--- index.php
+++ index.php
@@ -60,7 +60,7 @@ catch(PDOException $e){
 $applicationId = $db->lastInsertId();
 try {
   foreach ($_POST['favorite_languages'] as $languageId) {
-    $stmt = $db->prepare("INSERT INTO Application_languages (application_id, language_id) VALUES (?, ?)");
+    $stmt = $applicationId->prepare("INSERT INTO Application_languages (application_id, language_id) VALUES (?, ?)");
     $stmt->execute([$applicationId, $languageId]);
 }
 }
