c4786b529f8e834080f131e4a32b4ace251f2979
Lisower <144545336+Lisower@users.noreply.github.com>
2024-04-01 14:23:29 +0300

    Update script.js

--- script.js
+++ script.js
@@ -9,28 +9,7 @@ Button.addEventListener('click', () => {
 window.addEventListener('popstate', () => {
     Popup.style.display = 'none';
 });
- /*
-Form.addEventListener('submit', (e) => {
-    e.preventDefault();
 
-    const formData = new FormData(Form);
-    const httpRequest = new XMLHttpRequest();
-    httpRequest.open('POST', '', true);
-    httpRequest.onreadystatechange = () => {
-        if (httpRequest.readyState === XMLHttpRequest.DONE) {
-        if (httpRequest.status === 200) {
-            alert("Данные формы успешно отправлены!");
-            Form.reset();
-            localStorage.removeItem('FormData');
-            Popup.style.display = 'none';
-        } else {
-            alert("Что-то пошло не так! Данные не отправлены!");
-        }
-        }
-    };
-    httpRequest.send(formData);
-});
-*/
 Form.addEventListener('change', () => {
     const formData = new FormData(Form);
     const data = Object.fromEntries(formData.entries());
