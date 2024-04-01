fed4a21a2830d32e847f7c2f7c16c11d8329412d
Lisower <144545336+Lisower@users.noreply.github.com>
2024-04-01 14:26:02 +0300

    Update script.js

--- script.js
+++ script.js
@@ -9,24 +9,3 @@ Button.addEventListener('click', () => {
 window.addEventListener('popstate', () => {
     Popup.style.display = 'none';
 });
-
-Form.addEventListener('change', () => {
-    const formData = new FormData(Form);
-    const data = Object.fromEntries(formData.entries());
-localStorage.setItem('FormData', JSON.stringify(data));
-});
-
-window.addEventListener('load', () => {
-    const savedFormData = JSON.parse(localStorage.getItem('FormData'));
-
-    if (savedFormData) {
-        document.getElementById('FIO').value = savedFormData.FIO;
-        document.getElementById('e_mail').value = savedFormData.e_mail;
-        document.getElementById('phone_number').value = savedFormData.phone_number;
-        document.getElementById('birthday').value = savedFormData.birthday;
-        document.getElementById('sex').value = savedFormData.sex;
-        document.getElementById('favourite_languages').checked = savedFormData.favourite_languages;
-        document.getElementById('biography').checked = savedFormData.biography;
-        document.getElementById('check').checked = savedFormData.check;
-    }
-});
