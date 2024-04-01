fcb7a536f65d202696a6e4f285fb8da22dfa49ed
Lisower <144545336+Lisower@users.noreply.github.com>
2024-04-01 15:14:50 +0300

    Update index.php

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ if (empty($_POST['FIO'])) {
   $errors = TRUE;
 }
 
-if (!preg_match('/^[a-zа-яё]+$/', $_POST['FIO'])) {
+if (!preg_match('/^[a-zA-Z]+$/', $_POST['FIO'])) {
   print('ФИО введено некорректно!');
   $errors = TRUE;
 }
